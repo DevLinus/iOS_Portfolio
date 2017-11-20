5f95dd6e61d42169a2fa91ad03c1b20650bb32f7
Florian Middendorf <florianmiddendorf@Florians-MBP.fritz.box>
2017-11-20 20:31:27 +0100

    Add Meme Project

---
+++ Meme App/Meme App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme App
+//
+//  Created by Florian Middendorf on 20.11.17.
+//  Copyright © 2017 Florian Middendorf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
