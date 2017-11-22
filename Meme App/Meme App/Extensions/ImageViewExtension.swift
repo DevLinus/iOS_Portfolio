a4757fc7c531634359e5cda994c45295be15e9d0
Florian Middendorf <florianmiddendorf@Florians-MBP.fritz.box>
2017-11-22 19:58:24 +0100

    Add Code quelle

--- Meme App/Meme App/Extensions/ImageViewExtension.swift
+++ Meme App/Meme App/Extensions/ImageViewExtension.swift
@@ -2,7 +2,7 @@ import Foundation
 import UIKit
 
 extension UIImage{
-    
+//https://www.youtube.com/watch?v=wXDkZqmXVBs the code comes from this video.
     class func scaleImageToSize(imgage: UIImage, size: CGSize) ->UIImage{
         UIGraphicsBeginImageContext(size)
         imgage.draw(in: CGRect(origin: CGPoint.zero, size: size))
